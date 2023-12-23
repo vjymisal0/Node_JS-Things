204f33559f45de9e85cb860fb49744a2f17a8543
Vijay Misal <misalvijay153@gmail.com>
2023-12-23 11:30:19 +0530

    os module

--- index.js
+++ index.js
@@ -12,4 +12,16 @@
 // msg.sayHello()
 // msg.sayThanks()
 
+//os module
+// const os = require('os')
+// console.log(os.platform(), os.homedir())
+// console.log(os.arch())
+// console.log(os.hostname())
+// console.log(os.type())
+// console.log(os.release())
+// console.log(os.uptime())
+// console.log(os.userInfo())
+// console.log(os.cpus())
+// console.log(os.totalmem())
+
 
