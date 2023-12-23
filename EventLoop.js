4ec68b18dbb0a5506fc40db185a4926daacc26b9
Vijay Misal <misalvijay153@gmail.com>
2023-12-23 16:28:45 +0530

    git lens

--- EventLoop.js
+++ EventLoop.js
@@ -1,5 +1,6 @@
 function fun1() {
     setTimeout(() => console.log("I am Fun 1"), 2000)
+    process.nextTick(() => console.log("I am Process 1"))
 }
 function fun2() {
     console.log("I am Fun 2")
